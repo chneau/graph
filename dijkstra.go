15c80e89f6e5d8e4c4405d9a5c0349ea90bd386e
chneau <charles63500@gmail.com>
2020-02-06 11:47:58 +0000

    zi LDList of bogoss

--- dijkstra.go
+++ dijkstra.go
@@ -1,50 +1,84 @@
 package graph
 
 import (
-	"sort"
+	"container/list"
 )
 
-func DijkstraShortest(g Graph, from, to int) (int, []int) {
-	type VertexInfo struct {
-		Distance int
-		Path     []int
+type VertexInfo struct {
+	ID       int
+	Distance int
+	Path     []int
+}
+
+type DLList struct {
+	*list.List
+}
+
+func (l *DLList) InsertOrdered(v *VertexInfo) {
+	if l.Len() == 0 {
+		l.PushFront(v)
+		return
 	}
+	back := l.Back()
+	if back.Value.(*VertexInfo).Distance < v.Distance {
+		l.InsertAfter(v, back)
+		return
+	}
+	current := l.Front()
+	for current.Value.(*VertexInfo).Distance < v.Distance && current.Value.(*VertexInfo).ID != v.ID {
+		next := current.Next()
+		if next == nil {
+			break
+		}
+		current = next
+	}
+	if current.Value.(*VertexInfo).ID == v.ID {
+		return
+	}
+	l.InsertAfter(v, current)
+}
+
+func (l *DLList) PopFront() *VertexInfo {
+	e := l.Front()
+	l.Remove(e)
+	return e.Value.(*VertexInfo)
+}
+
+func DijkstraShortest(g Graph, from, to int) (int, []int) {
 	vertices := map[int]*VertexInfo{
-		from: &VertexInfo{Path: []int{from}},
+		from: &VertexInfo{ID: from, Path: []int{from}},
 	}
 	visited := map[int]bool{}
-	toVisit := []int{from}
+	toVisit := DLList{List: list.New()}
+	toVisit.InsertOrdered(vertices[from])
 	found := false
-	for len(toVisit) > 0 && !found {
-		visiting := toVisit[0] // take the first to visit
-		for _, k := range g[visiting].Order {
+	for toVisit.Len() > 0 && !found {
+		visiting := toVisit.PopFront() // take the first to visit
+		for _, k := range g[visiting.ID].Order {
 			if visited[k] { // don't visit fully visited
 				continue
 			}
-			v := g[visiting].Neighbours[k]
-			lp := len(vertices[visiting].Path)
+			v := g[visiting.ID].Neighbours[k]
+			lp := len(vertices[visiting.ID].Path)
 			if _, exist := vertices[k]; !exist { // if doenst exist, add vertex
 				vertices[k] = &VertexInfo{
-					Distance: v + vertices[visiting].Distance,
-					Path:     append(vertices[visiting].Path[:lp:lp], k),
+					ID:       k,
+					Distance: v + vertices[visiting.ID].Distance,
+					Path:     append(vertices[visiting.ID].Path[:lp:lp], k),
 				}
-				toVisit = append(toVisit, k)
+				toVisit.InsertOrdered(vertices[k])
 			} else {
-				newDistance := v + vertices[visiting].Distance
+				newDistance := v + vertices[visiting.ID].Distance
 				if vertices[k].Distance > newDistance { // update only if better
 					vertices[k].Distance = newDistance
-					vertices[k].Path = append(vertices[visiting].Path[:lp:lp], k)
+					vertices[k].Path = append(vertices[visiting.ID].Path[:lp:lp], k)
 				}
 			}
 		}
-		if visiting == to {
+		if visiting.ID == to {
 			break
 		}
-		toVisit = toVisit[1:]                     // remove from tovisit
-		visited[visiting] = true                  // mark visited
-		sort.Slice(toVisit, func(i, j int) bool { // sort tovisit
-			return vertices[toVisit[i]].Distance < vertices[toVisit[j]].Distance
-		})
+		visited[visiting.ID] = true // mark visited
 	}
 	if _, exist := vertices[to]; !exist {
 		return 0, nil
