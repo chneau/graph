9a519f64055225add95c19331af398ad78fddef8
chneau <charles63500@gmail.com>
2020-02-07 13:48:26 +0000

    ok

--- simplify.go
+++ simplify.go
@@ -50,6 +50,52 @@ func simplify(g Graph) {
 			continue
 		}
 		to := g[mid].Order[0]
+		simplifyVertices(g, from, mid, to)
+		delete(g, mid)
+	}
+}
+
+func BiSimplify(g Graph) {
+	l := len(g)
+	for {
+		biSimplify(g)
+		if l == len(g) {
+			break
+		}
+		l = len(g)
+	}
+}
+func biSimplify(g Graph) {
+	optimisable := map[int]bool{}
+	where := map[int][]int{}
+	for k, v := range g {
+		if len(v.Order) == 2 { // if edge only going to two vertices
+			optimisable[k] = true
+		}
+		for _, i := range v.Order { // map where a vertex appear
+			where[i] = append(where[i], k)
+		}
+	}
+	for k := range optimisable {
+		if len(where[k]) != 2 { // remove vertex with multiple parrents
+			delete(optimisable, k)
+		}
+	}
+	for k := range where {
+		if _, exist := optimisable[k]; !exist { // remove useless data on where map
+			delete(where, k)
+		}
+	}
+	for mid := range optimisable {
+		from := where[mid][0]
+		if mid == from { // a round has been reduced
+			RemoveEdge(g[from], mid)
+			continue
+		}
+		if _, exist := g[from]; !exist {
+			continue
+		}
+		to := g[mid].Order[0]
 		cost1 := g[mid].Neighbours[to]
 		cost2 := g[from].Neighbours[mid]
 		cost := cost1 + cost2
@@ -57,4 +103,19 @@ func simplify(g Graph) {
 		RemoveEdge(g[from], mid)
 		g[from].AddEdge(to, cost)
 	}
+}
+
+func simplifyVertices(g Graph, f, m, t int) bool {
+	if _, exist := g[f].Neighbours[m]; !exist {
+		return false
+	}
+	if _, exist := g[m].Neighbours[t]; !exist {
+		return false
+	}
+	cost1 := g[f].Neighbours[m]
+	cost2 := g[m].Neighbours[t]
+	cost := cost1 + cost2
+	RemoveEdge(g[f], m)
+	g[f].AddEdge(t, cost)
+	return true
 }
